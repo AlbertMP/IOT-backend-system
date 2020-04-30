93d0583e6eb398f6fcf50ef4db7595fdb3705855
Miao Peng <2570869891@qq.com>
2020-04-30 16:17:30 +0800

    add mqtt related contants

--- src/main/java/com/cl/common/Contants.java
+++ src/main/java/com/cl/common/Contants.java
@@ -4,4 +4,8 @@ public class Contants {
 
     public static final String USER_SESSION = "USER_SESSION";
 
+    // 定义云平台的上行和下行通道
+    public static final String MQTT_UP = "_UP";
+    public static final String MQTT_DW = "_DW";
+
 }
